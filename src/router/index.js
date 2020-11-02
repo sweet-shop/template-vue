43280eae31283071e02fec5ebbb43c2ce427ee65
huarxia <liubiao0810@live.cn>
2020-11-02 10:14:12 +0800

    docs: 🍭 修改文档

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import routes from './routers';
-import store from '../store';
+// import store from '../store';
 Vue.use(Router);
 const router = new Router({
     routes
@@ -11,7 +11,7 @@ router.beforeEach((to, from, next) => {
     // 路由发生变化修改页面title
     if (to.meta.title) {
         document.title = to.meta.title;
-        store.commit('SET_META', to.meta);
+        // store.commit('SET_META', to.meta);
     }
     next();
 });
